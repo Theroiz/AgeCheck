394810270679bac2092364126df48fe813c29266
Theroiz <164636377+Theroiz@users.noreply.github.com>
2024-06-18 21:25:46 -0300

    Add files via upload

---
+++ src/AgeCheck.java
@@ -0,0 +1,15 @@
+public class AgeCheck {
+   
+    public static void main(String[] args) {
+        int myAge = 16;   // I am 19; let me see if I can drive yet
+        
+        if(myAge >= 16) {
+            System.out.println("Tenho idade suficiente para ter uma carteira de motorista!");
+        }
+        else {
+            System.out.println("Nao tenho idade suficiente");
+        }
+    }
+}
+    
+
